b1d6bde2e0206c8b261a91aab39cd68a7741af89
Charles Sima <crlessima@gmail.com>
2026-08-01 10:14:58 -0300

    Corrigindo alias @ para build da Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  webpack: (config) => {
+    config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
