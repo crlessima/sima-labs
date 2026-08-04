489cb6fe6082648b4592dfb4e5bb9048d4be3135
Charles Sima <crlessima@gmail.com>
2026-08-04 20:54:06 -0300

    Correção adsense

--- src/components/ads/Adsense.tsx
+++ src/components/ads/Adsense.tsx
@@ -1,23 +1,35 @@
 "use client";
-import { useEffect } from "react";
 
-export default function AdBanner() {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.error("Erro ao carregar AdSense:", err);
+declare global {
+    interface Window {
+        adsbygoogle: any[];
     }
-  }, []);
+}
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block", textAlign: "center", margin: "20px 0" }}
-      data-ad-client="ca-pub-3116341116644314"
-      data-ad-slot="6865347702"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
-  );
+import { useEffect, useState } from "react";
+
+export default function Adsense() {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            try {
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+                setLoaded(true);
+            } catch (err) {
+                console.error("Erro ao carregar AdSense:", err);
+            }
+        }
+    }, [loaded]);
+
+    return (
+        <ins
+            className="adsbygoogle"
+            style={{ display: "block", margin: "20px 0" }}
+            data-ad-client="ca-pub-3116431116643414"
+            data-ad-slot="6635477072"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+        ></ins>
+    );
 }
