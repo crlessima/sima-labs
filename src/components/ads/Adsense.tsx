82f4f5bcee23f608525cce45201a9659df55779e
Charles Sima <crlessima@gmail.com>
2026-08-04 20:48:32 -0300

    Correção adsense

--- src/components/ads/Adsense.tsx
+++ src/components/ads/Adsense.tsx
@@ -1,27 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-export default function Adsense() {
-  const [loaded, setLoaded] = useState(false);
-  declare global {
-    interface Window {
-      adsbygoogle: any[];
-    }
-  }
-
+export default function AdBanner() {
   useEffect(() => {
-    if (!loaded) {
-      try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-        setLoaded(true);
-      } catch (e) {}
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error("Erro ao carregar AdSense:", err);
     }
-  }, [loaded]);
+  }, []);
 
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block", margin: "20px 0" }}
+      style={{ display: "block", textAlign: "center", margin: "20px 0" }}
       data-ad-client="ca-pub-3116341116644314"
       data-ad-slot="6865347702"
       data-ad-format="auto"
